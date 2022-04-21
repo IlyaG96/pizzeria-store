4b7db989d013f5a746f54db6ea0d3d1a6ecaa2fe
IlyaG96 <ilyag_96@icloud.com>
2022-04-22 09:36:16 +1100

    add func to work with yandex geo api

--- bot_tools.py
+++ bot_tools.py
@@ -1,5 +1,5 @@
 from textwrap import dedent
-
+import requests
 
 def format_product_description(product_description):
     product_description = product_description['data']
@@ -74,3 +74,21 @@ def build_menu(buttons, n_cols,
         for button in footer_buttons:
             menu.append(button)
     return menu
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
+    return lat, lon
