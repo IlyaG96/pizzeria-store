4f74641f415ea5ab67f721ce0350eaeae8858bfe
IlyaG96 <ilyag_96@icloud.com>
2022-04-26 10:50:09 +1100

    move related to geoposition funcs to geo_api

--- bot_tools.py
+++ bot_tools.py
@@ -1,6 +1,4 @@
 from textwrap import dedent
-import requests
-from geopy.distance import distance as dist
 
 
 def format_product_description(product_description):
@@ -74,45 +72,3 @@ def build_menu(buttons, n_cols,
         for button in footer_buttons:
             menu.append(button)
     return menu
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(' ')
-    return lat, lon
-
-
-def get_distance(pizzeria):
-    return pizzeria['distance']
-
-
-def show_nearest_pizzeria(pizzerias, user_location):
-
-    for pizzeria in pizzerias:
-        distance = dist(
-            pizzeria['coordinates'], user_location,
-        ).km
-        pizzeria['distance'] = distance
-    nearest_pizzeria = min(pizzerias, key=get_distance)
-
-    return nearest_pizzeria
-
-
-def send_notification(context):
-    text = "Приятного аппетита! *место для рекламы сообщение что делать если пицца не пришла"
-    job = context.job
-    context.bot.send_message(
-        job.context, text=text
-    )
